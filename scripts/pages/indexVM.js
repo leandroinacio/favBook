cea18fcabe1ddd8cbf8659a0c114c1eafdd20192
leandroinacio <leandroinacio@terra.com.br>
2016-10-15 09:40:03 -0300

    New updates

--- scripts/pages/indexVM.js
+++ scripts/pages/indexVM.js
@@ -4,58 +4,32 @@ var indexModule = (function() {
 		var self = this;
 		try {
 			//Forms the page strucure
-			self.paragraphs = ko.observableArray([]);
+			self.posts = ko.observableArray([]);
 
 			//Search item
 			self.searchItem = ko.observable('');
 
-			// var xmlHTTP = new XMLHttpRequest();
-			// xmlHTTP.open("GET", "str1.xml", false);
-			// xmlHTTP.send(null);
-			
-			// debugger;
-
-			// var client = new XMLHttpRequest();
-			// client.open('GET', '/foo.txt');
-			// client.onreadystatechange = function() {
-			//   alert(client.responseText);
-			// }
-			// client.send();
-
-			// debugger;
-			//Get the page structure
-			// $.get( "http://localhost/favBook.git/trunk/scripts/pages/str1.xml", "", self.formatPage, "xml");
-
 			//Format it into observables
-			// self.formatPage = function(data)
-			// {
-			// 	try 
-			// 	{
-			// 		debugger;
+			self.retrievePosts = function(data)
+			{
+				try 
+				{
+					$.getJSON("http://localhost:8080/post/list", function(posts) { 
 
-			// 		$.ajax({
-			// 		    type: "GET",
-			// 		    url: "something.xml",
-			// 		    dataType: "xml",
-			// 		    success: function (xml) {
+						// Setup post objects and add to list
+						posts.map(function(post) {
+							self.posts.push(new postModule.Post(post));
+						});
 
-			// 		        // Parse the xml file and get data
-			// 		        var xmlDoc = $.parseXML(xml),
-			// 		            $xml = $(xmlDoc);
-			// 		        $xml.find('category[name="My t"] logo').each(function () {
-			// 		            $("#news-container").append($(this).text() + "<br />");
-			// 		        });
-			// 		    }
-			// 		});
-			// 		//debugger;
-			// 		//try $.parseXML( xml );
-			// 	}catch(e)
-			// 	{
-			// 		console.log(e)
-			// 	}
-			// };
+					});
+				}catch(e)
+				{
+					console.log(e)
+				}
+			};
 
-			// self.formatPage();
+			// Fetch data
+			self.retrievePosts();
 		}
 		catch(e) 
 		{
